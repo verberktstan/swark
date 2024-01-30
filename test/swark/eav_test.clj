e68e2cc636beec0c4070c8c97e93317decbe3ef4
Stan Verberkt <2913270+verberktstan@users.noreply.github.com>
2024-01-30 06:58:47 +0100

    Update eav_test.clj

--- test/swark/eav_test.clj
+++ test/swark/eav_test.clj
@@ -20,22 +20,22 @@
 
 (t/deftest parse-row
   (t/is (= [{:entity/attribute :id :entity/value 1 :attribute :username :value "Arnold"}]
-           (map sut/parse-row (sut/->rows :id USER))))
+           (map #'sut/parse-row (sut/->rows :id USER))))
   (t/is (= [{:entity/attribute :user/id :entity/value 2 :attribute :user/name :value "Arnold"}
             {:entity/attribute :user/id :entity/value 2 :attribute :user/city :value "Birmingham"}]
-           (map sut/parse-row (sut/->rows :user/id USER2))))
+           (map #'sut/parse-row (sut/->rows :user/id USER2))))
   (t/is (= [{:entity/attribute :id :entity/value 1 :attribute "username" :value "Arnold"}]
-           (map (partial sut/parse-row {:attribute name}) (sut/->rows :id USER))))
+           (map (partial #'sut/parse-row {:attribute name}) (sut/->rows :id USER))))
   (t/is (= [{:entity/attribute :user/id :entity/value :two :attribute "name" :value "Arnold"}
             {:entity/attribute :user/id :entity/value :two :attribute "city" :value "Birmingham"}]
-           (map (partial sut/parse-row {:entity/value {2 :two} :attribute name}) (sut/->rows :user/id USER2)))))
+           (map (partial #'sut/parse-row {:entity/value {2 :two} :attribute name}) (sut/->rows :user/id USER2)))))
 
 (t/deftest filter-eav
   (let [eav1 {:entity/attribute :id
               :entity/value 1
               :attribute :username
               :value "Arnold"}]
-    (t/is (true? (sut/filter-eav {:attribute #{:username} :value #{"Arnold"}} eav1)))
-    (t/is (false? (sut/filter-eav {:attribute #{:username} :value #{"Zorro"}} eav1)))
-    (t/is (true? (sut/filter-eav {:entity/attribute #{:id} :entity/value #{1}})))
-    (t/is (false? (sut/filter-eav {:entity/attribute #{:id} :entity/value #{2}})))))
+    (t/is (true? (#'sut/filter-eav {:attribute #{:username} :value #{"Arnold"}} eav1)))
+    (t/is (false? (#'sut/filter-eav {:attribute #{:username} :value #{"Zorro"}} eav1)))
+    (t/is (true? (#'sut/filter-eav {:entity/attribute #{:id} :entity/value #{1}})))
+    (t/is (false? (#'sut/filter-eav {:entity/attribute #{:id} :entity/value #{2}})))))
