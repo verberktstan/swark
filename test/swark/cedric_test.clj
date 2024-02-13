fe4dd95ca6fdcb3e7409e15c77250465c583a4fa
Stan Verberkt <verberktstan@gmail.com>
2024-02-13 13:14:20 +0100

    Don't override :user/id in tests

--- test/swark/cedric_test.clj
+++ test/swark/cedric_test.clj
@@ -11,33 +11,33 @@
   edn/read-string)
 
 ;; Parse user record's gender value as a keyword
-(remove-method sut/value-parser [:user/id :user/gender])
-(defmethod sut/value-parser [:user/id :user/gender] [_]
+(remove-method sut/value-parser [::user-id :user/gender])
+(defmethod sut/value-parser [::user-id :user/gender] [_]
   keyword)
 
 ;; Parse primary-value as clojure object (int)
-(remove-method sut/primary-value-parser :user/id)
+(remove-method sut/primary-value-parser ::user-id)
 (remove-method sut/primary-value-parser :id)
-(defmethod sut/primary-value-parser :user/id [_]
+(defmethod sut/primary-value-parser ::user-id [_]
   edn/read-string)
 (defmethod sut/primary-value-parser :id [_]
   edn/read-string)
 
 (deftest pipeline-test
   (let [user1 {:id 1 :username "Stan" :category 1}
-        user2 #:user{:id 2 :name "Nats" :gender :unknown}
+        user2 {::user-id 2 :user/name "Nats" :user/gender :unknown}
         rows1 (#'sut/serialize {:primary-key :id} user1)
-        rows2 (#'sut/serialize {:primary-key :user/id} user2)]
+        rows2 (#'sut/serialize {:primary-key ::user-id} user2)]
     (are [result rows] (= result (->> rows
                                       (map (partial drop 1))
                                       (map (partial take 4))))
       [["id" "1" "username" "Stan"]
        ["id" "1" "category" "1"]]               rows1
-      [["user/id" "2" "user/name" "Nats"]
-       ["user/id" "2" "user/gender" "unknown"]] rows2)
+      [["swark.cedric-test/user-id" "2" "user/name" "Nats"]
+       ["swark.cedric-test/user-id" "2" "user/gender" "unknown"]] rows2)
     (is
      (= [{:username "Stan" :id 1 :category 1}
-         #:user{:name "Nats" :id 2 :gender :unknown}]
+         {:user/name "Nats" ::user-id 2 :user/gender :unknown}]
         (#'sut/merge-rows (concat rows1 rows2))))))
 
 (def ^:private NAMES #{"Alfa" "Bravo" "Charlie" "Delta" "Echo" "Foxtrot" "Golf" "Hotel" "India" "Juliett" "Kilo" "Lima" "Mike" "November" "Oscar" "Papa" "Quebec" "Romeo" "Sierra" "Tango" "Uniform" "Victor" "Whiskey" "X-ray" "Yankee" "Zulu"})
